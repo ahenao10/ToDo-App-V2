d47412bce7f90fe5e20a94a20dee1e11f7a7bf1a
ahenao10 <alejandro10.dev@gmail.com>
2025-03-14 15:25:38 +0100

    Se prepara back para despliegue en render

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
@@ -59,6 +59,6 @@ app.get('/stadistics', (req, res) => {
   console.log('Stadistics sent!');
 });
 
-app.listen(PORT, (err) => {
+app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
