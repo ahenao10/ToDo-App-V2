dcfdbd2a30bc79e32ad6be5017f67e2dc6e91002
ahenao10 <alejandro10.dev@gmail.com>
2025-03-15 01:34:50 +0100

    Se añade manejador de errores en Dashboard Modal por error de mostrado de estadisticas si no hay nada en base

--- frontend/src/Modals/DashboardModal/index.js
+++ frontend/src/Modals/DashboardModal/index.js
@@ -9,11 +9,11 @@ function Dashboard({ setOpenDashboard }) {
 
     const { todoStadisticsMonth, todoStadisticsYear } = useServerDashboard()
 
-    const dataCreated = todoStadisticsMonth.map((data) => data.created)
+    const dataCreated = todoStadisticsMonth.map((data) => data.created) || 0
 
-    const dataCompleted = todoStadisticsMonth.map((data) => data.completed)
+    const dataCompleted = todoStadisticsMonth.map((data) => data.completed) || 0
 
-    const dataDeleted = todoStadisticsMonth.map((data) => data.deleted)
+    const dataDeleted = todoStadisticsMonth.map((data) => data.deleted) || 0 // Se obtienen los datos de los días del mes
 
     const months = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
 
